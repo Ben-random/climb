176959c543690ea9c426581f8346f019aef4e551
Alicia Sykes <alicia@omg.lol>
2026-01-26 16:33:58 +0000

    fix: Failing installastion, creates missing dir, fixes go version (#28)
    * fix: Failing installastion
    
    * Fix: Make idempotent by updating alias instead of create if already exist
    
    ---------
    
    Co-authored-by: Ben Girvan <benjamin.girvan@icloud.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 
 # Build executable
+mkdir -p ./bin
 go build -o ./bin/climb
 
 # Grant execute perms to path set up script
@@ -9,5 +11,13 @@ chmod +x ./scripts/setUpPath.sh
 # Run paths set up script
 ./scripts/setUpPath.sh
 
-# Create command for climb CLI
-./bin/climb create climb ./bin/climb
+# Apply changes to ~/.zshrc
+touch ~/.zshrc
+
+# Create or update command for climb CLI
+if command -v climb >/dev/null 2>&1; then
+ echo "Updating existing climb installation..."
+ ./bin/climb update climb ./bin/climb
+else
+ ./bin/climb create climb ./bin/climb
+fi
